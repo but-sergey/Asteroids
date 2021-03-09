e66e908418630ca005c8fc01b48a3091e5baa76c
but-sergey <but-sergey@bk.ru>
2021-03-09 20:34:09 +0300

    Скрипты

---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+
+namespace Asteroids
+{
+    internal sealed class HealthSystem
+    {
+        private readonly float _maxHp = 100.0f;
+        private float _hp;
+
+        public HealthSystem(float maxHp)
+        {
+            _maxHp = maxHp;
+            _hp = _maxHp;
+        }
+
+        public bool Hurt(GameObject gameObject)
+        {
+            _hp--;
+            return (_hp > 0);
+        }
+
+        public void Recover()
+        {
+            _hp = _maxHp;
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+
+namespace Asteroids
+{
+    internal sealed class Player : MonoBehaviour
+    {
+        [SerializeField] private float _speed = 1.0f;
+        [SerializeField] private float _maxHp = 100.0f;
+        [SerializeField] private Rigidbody2D _bullet;
+        [SerializeField] private Transform _barrel;
+        [SerializeField] private float _force;
+
+        private MoveTransform _moveTransform;
+        private PlayerInput _playerInput;
+        //private FireSystem _fireSystem;
+        private HealthSystem _healthSystem;
+
+        private void Start()
+        {
+            _moveTransform = new MoveTransform(transform, _speed);
+            _playerInput = new PlayerInput();
+            //_fireSystem = new FireSystem(_bullet, _barrel);
+            _healthSystem = new HealthSystem(_maxHp);
+        }
+
+        private void Update()
+        {
+            var move = _playerInput.GetBaseMove();
+            _moveTransform.Move(move, Time.deltaTime);
+
+            if (_playerInput.FireInput())
+            {
+                Debug.Log("Fire!");
+                //_fireSystem.Fire(_force);
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (!_healthSystem.Hurt(other.gameObject))
+                Destroy(gameObject);
+        }
+    }
+}
